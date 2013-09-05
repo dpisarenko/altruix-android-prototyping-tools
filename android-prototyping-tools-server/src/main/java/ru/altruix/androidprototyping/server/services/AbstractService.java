88380c0fb5a6c506d8426e8662b3498c34f898b2
Dmitri Pisarenko <dp@altruix.co>
2013-09-05 17:38:22 +0400

    Implementing reflection.

--- android-prototyping-tools-server/src/main/java/ru/altruix/androidprototyping/server/services/AbstractService.java
+++ android-prototyping-tools-server/src/main/java/ru/altruix/androidprototyping/server/services/AbstractService.java
@@ -5,6 +5,11 @@
 package ru.altruix.androidprototyping.server.services;
 
 import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,11 +27,22 @@ import ru.altruix.androidprototyping.server.persistence.IPersistence;
  *
  */
 public abstract class AbstractService<PersistenceClass extends IPersistence,
-	RequestClass, ResponseClass> {
+	RequestClass, ResponseClass extends AbstractResponse> {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractService.class);
 
 	private PersistenceClass persistence;
-
+	
+	private Class<?> requestClass;
+	private Class<?> responseClass;
+	
+	public AbstractService()
+	{
+		final Map<String,Type> genericTypes = getGenericTypes();
+		
+		requestClass = genericTypes.get("RequestClass").getClass();
+		responseClass = genericTypes.get("ResponseClass").getClass();
+	}
+	
 	public PersistenceClass getPersistence() {
 		return persistence;
 	}
@@ -37,32 +53,57 @@ public abstract class AbstractService<PersistenceClass extends IPersistence,
 	
 	public String processRequest(final String aRequestAsText)
 	{
-		try {
-			final ObjectMapper mapper = new ObjectMapper();
-			
-			final RequestClass request = mapper.readValue(aRequestAsText, 
-					RequestClass);
+		@SuppressWarnings("unchecked")
+		final ResponseClass response = (ResponseClass) responseClass.newInstance();
+		
+		response.setRequestProcessedSuccessfully(false);
+		final ObjectMapper mapper = new ObjectMapper();
 
+		try {						
+			final RequestClass request = mapper.readValue(aRequestAsText, 
+					requestClass);
 			
-			final ResponseClass response = null;
-						
+			fillResponseWithData(request, response);
+			response.setRequestProcessedSuccessfully(true);			
 			return mapper.writeValueAsString(response);
 		} catch (final JsonParseException exception) {
-			LOGGER.error("getUserId", exception);
+			LOGGER.error("", exception);
+			return mapper.writeValueAsString(response);
 		} catch (final JsonMappingException exception) {
-			LOGGER.error("getUserId", exception);
+			LOGGER.error("", exception);
+			return mapper.writeValueAsString(response);
 		} catch (final IOException exception) {
-			LOGGER.error("getUserId", exception);
+			LOGGER.error("", exception);
+			return mapper.writeValueAsString(response);
 		} 
 		catch (final InterruptedException exception) {
-			LOGGER.error("getUserId", exception);		
+			LOGGER.error("", exception);		
 		}
 
-		return "";
+		return mapper.writeValueAsString(response);
 		
-		
-		final String response = "";
-		return response;
 	}
+
+	private Map<String,Type> getGenericTypes()
+	{
+		final Map<String,Type> result = new HashMap<String,Type>();
+		
+		@SuppressWarnings("rawtypes")
+		final Class clazz = getClass();
+		
+		clazz.getTypeParameters();
+		
+		final ParameterizedType gen = (ParameterizedType) clazz.getGenericSuperclass();
+		final TypeVariable<?> typeVars[] = clazz.getTypeParameters();
+		final Type [] types = gen.getActualTypeArguments();
+	    for (int i = 0; i < typeVars.length; i++) {
+	    	result.put(typeVars[i].getName(), types[i]);
+	    }
+		
+		return result;
+	}
+	
+	protected abstract void fillResponseWithData(final RequestClass aRequest,
+			final ResponseClass aResponse);
 }
 
